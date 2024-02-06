a5ace83a3efd076284443b4c088c600c11a02d44
kozyavas <ozykenan@gmail.com>
2024-02-06 22:31:23 +0100

    kenan 06.02.2024

---
+++ src/test/java/Kenan.java
@@ -0,0 +1,5 @@
+public class Kenan {
+    public static void main(String[] args) {
+        System.out.println("Benim class");
+    }
+}
--- src/test/java/OrtakClass.java
+++ src/test/java/OrtakClass.java
@@ -1,5 +1,8 @@
 public class OrtakClass {
     public static void main(String[] args) {
         System.out.println("Hi, I'm Here");
+
+        System.out.println("bu satir musaitmi?");
     }
+
 }
