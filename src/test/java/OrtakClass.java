8de09a10f583e1556c43e817aaab6c4f4f609352
yakicy <153319661+yakicy@users.noreply.github.com>
2024-02-06 23:13:36 +0100

    YAKUP 06.02.2024

--- src/test/java/OrtakClass.java
+++ src/test/java/OrtakClass.java
@@ -1,8 +1,14 @@
 public class OrtakClass {
     public static void main(String[] args) {
-        System.out.println("Hi, I'm Here");
 
-        System.out.println("bu satir musaitmi?");
+        System.out.println("yakup123");
+
+
+        System.out.println("Yakup github");
+
+
+
+
     }
 
 }
---
+++ src/test/java/Yakup.java
@@ -0,0 +1,7 @@
+public class Yakup {
+    public static void main(String[] args) {
+
+
+        System.out.println("GitHub calisma");
+    }
+}
