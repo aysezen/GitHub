9c310cd8abd983333309d08d721599b742cd76d2
kozyavas <ozykenan@gmail.com>
2024-02-06 23:33:15 +0100

    kenan 06.02.2024 v2

--- src/test/java/OrtakClass.java
+++ src/test/java/OrtakClass.java
@@ -2,7 +2,8 @@ public class OrtakClass {
     public static void main(String[] args) {
 
         System.out.println("yakup123");
-
+        System.out.println("kenan1");
+        System.out.println("kenan2");
 
         System.out.println("Yakup github");
 
